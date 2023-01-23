41a1c3eb3ae2ea5332b51b7d6045618447aab4a3
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-01-23 13:41:38 +0800

    add icons

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     <title>TODO</title>
 </head>
 
@@ -16,6 +18,7 @@
         <button type="submit" name="submit">Add
         </button>
     </form>
+
     <?php
     $sql = "SELECT * FROM todos";
     $query = mysqli_query($conn, $sql);
