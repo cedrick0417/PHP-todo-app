2a81be561d2a85fbdd540a9d7d9035d605f370ef
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-01-23 14:08:38 +0800

    delete task

---
+++ delete.php
@@ -0,0 +1,14 @@
+
+<?php include "./db.php"; ?>
+<!-- get the id from the url -->
+<?php
+if (isset($_GET["id"])) {
+    $TodoID = $_GET['id']; //declare a TodoID variable from the Query String id
+    // prepare SQL statement to delete Todo
+    $sql = "DELETE FROM todos WHERE id = '$TodoID'";
+    $query = mysqli_query($conn, $sql);
+    if ($query) {
+        echo 'Todo deleted successfully';
+    }
+}
+?>
