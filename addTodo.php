9f4b406233349714b060843b9470ee6a2918afc5
Cedrick Garcia <cedrick.garcia@ippphilippines.com>
2023-01-23 12:06:39 +0800

    add todo

---
+++ addTodo.php
@@ -0,0 +1,20 @@
+
+<?php  include './db.php'; ?>
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $inputData = mysqli_real_escape_string($conn, $_POST['todo-input']);
+
+    $sql = "INSERT INTO todos SET task = '$inputData'";
+
+    $query = mysqli_query($conn,$sql);
+
+    if($query){
+        echo 'Todo added succesfully ';
+
+        echo $inputData;  
+    }
+     
+}
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO</title>
+</head>
+
+<body>
+    <h1>Todo</h1>
+    <form action="addTodo.php" method="POST">
+        <input type="text" name="todo-input" placeholder="add a todo">
+        <button type="submit" name="submit">Add
+        </button>
+    </form>
+</body>
+
+</html>
